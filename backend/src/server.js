aaa11d3c7e179ebc1638e6391bcabcaa50c1764e
Kashika Ghosh <kashika0613@gmail.com>
2026-08-09 15:27:15 +0530

    making ready for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,22 @@ const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const authroutes = require("./routes/auth.js");
-const msg = require("./routes/msg.js")
+const msg = require("./routes/msg.js");
+const path = require("path");
 
-app.use("/api/auth",authroutes);
-app.use("/api/msg",msg);
+
+
+app.use("/api/auth", authroutes);
+app.use("/api/msg", msg);
+
+// To make server work in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*path", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
 app.listen(PORT, () => {
- console.log(`server working on port ${PORT}`);
-}) 
+    console.log(`Server working on port ${PORT}`);
+});
